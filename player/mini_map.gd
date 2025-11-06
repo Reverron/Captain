5b8caed7bad68518a37c63f6c6490b01c2451855
Eric Li <15696082812@163.com>
2025-11-06 23:45:16 +0100

    save

--- player/mini_map.gd
+++ player/mini_map.gd
@@ -4,53 +4,37 @@ class_name MiniMap
 @onready var background: TextureRect = %Background
 
 @export var player: Player
-var pois: Array[Node2D] = []
-var markers: Array[Sprite2D] = []
+var player_marker
+var markers := {}
 
-var map_scale
-@export var scan_radius := 50.0
+@export var scan_radius := 200.0
 @export var zoom := 1.0
 
 func _ready() -> void:
 	init_player_marker()
 	get_minimap_objs()
-	#size = Vector2(scan_radius, scan_radius)
-	#map_scale = background.get_rect().size / (get_viewport_rect().size * zoom)
+	#background.size = Vector2(scan_radius, scan_radius)
+	#background.set_anchors_preset(Control.PRESET_CENTER)
 
-#func _physics_process(_delta: float) -> void:
-	#if player: position = Vector2(player.position.x, player.position.y)
-	#var player_pos = player.global_transform.origin if player else Vector2.ZERO
-	#for poi in pois:
-		#var poi_pos = poi.global_transform.origin
-		#var _offset = Vector2(poi_pos.x, poi_pos.y) - Vector2(player_pos.x, player_pos.y)
-		#var distance = _offset.length()
-		#if distance > scan_radius / 2:
-			## Clamp to minimap edge
-			##var clamped_offset = _offset.normalized() * (scan_radius / 2 - 3.0)
-			##poi.icon.global_transform.origin = Vector2(
-				##player_pos.x + clamped_offset.x,
-				##player_pos.y + clamped_offset.y)
-			##poi.icon.scale = Vector2(2, 2)
-			#poi.icon.hide()
-		#else:
-			#poi.icon.show()
-			#poi.icon.global_transform.origin = Vector2(
-				#poi_pos.x,
-				#poi_pos.y)
-			#poi.icon.scale = Vector2(2, 2)
+func _physics_process(_delta: float) -> void:
+	if player_marker: player_marker.rotation = player.rotation + PI / 2
+	for obj in markers:
+		var obj_pos = obj.global_position - player.global_position + Vector2(get_window().size / 2)
+		markers[obj].position = obj_pos
 
 func init_player_marker():
 	if not player: 
 		return
-	var player_marker = player.get_node("RadarObjComponent")
+	player_marker = player.get_node("RadarObjComponent")
 	if player_marker:
 		player_marker.show()
 		player_marker.global_position = get_window().size / 2
 
 func get_minimap_objs():
 	var map_objs = get_tree().get_nodes_in_group("radar_objs")
-	for o in map_objs:
-		var marker = o.get_node("RadarObjComponent")
-		marker.reparent(background)
-		marker.show()
-		markers.append(marker)
+	for obj in map_objs:
+		var marker = obj.get_node("RadarObjComponent")
+		if marker:
+			marker.reparent(background)
+			marker.show()
+			markers[obj] = marker
