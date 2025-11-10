a7a5c712fe7ec1ff1a0de18e27e17d18db47780c
Reverron <sk8reverron@gmail.com>
2025-11-10 18:26:39 +0100

    credits page

--- level/game_control.gd
+++ level/game_control.gd
@@ -5,13 +5,15 @@ class_name GameControl
 @onready var select_marker: SelectMarker = %SelectMarker
 @onready var mini_map: MiniMap = %MiniMap
 @onready var side_screen: SideScreen = %SideScreen
+@onready var retro_effect_rect: ColorRect = %RetroEffectRect
 
 var in_main_menu := false
 var in_game := false
 
 func _ready() -> void:
 	Global.game_controller = self
-	set_game_content()
+	#set_game_content()
+	set_game_menu_content(0)
 
 func set_game_menu_content(menu_id: int):
 	if not in_main_menu:
@@ -21,6 +23,7 @@ func set_game_menu_content(menu_id: int):
 		Global.radar_controller.can_control = false
 		in_main_menu = true
 		in_game = false
+		retro_effect_rect.show()
 	mini_map.set_menu(menu_id)
 
 func set_game_content():
@@ -31,4 +34,5 @@ func set_game_content():
 		Global.radar_controller.can_control = true
 		in_game = true
 		in_main_menu = false
+		retro_effect_rect.hide()
 	mini_map.set_game()
